95d16caca565a8b04cd82f192672a67d4a6e5163
dziuba <dziuba@agmk.net>
2014-07-07 17:16:37 +0200

    Pierwsze testy

---
+++ TO_DO.md
@@ -0,0 +1,17 @@
+Lista rzeczy do zrobienia
+=========================
+A. Import produktów do sklepu
+
+1. Otwórz plik sm.csv
+2. Wczytaj parametry do tablicy
+3. Rozszyfruj nazwę produktu i ean. Przygotuj grupę jeżeli trzeba.
+4. Połącz się z bazą i sprawdź czy produkt jest już w bazie.
+5. Jeżeli tak to aktualizuj jego stan/nazwę/itd
+6. Jeżeli nie to go dodaj sprawdzając czy ma już swoją grupę.
+
+B. Export zamówień
+
+1. Wczytaj zamówienia do przyjęciaz bazy danych.
+2. Wczytaj istniejący plik zamówień.
+3. Sparuj obie tablice.
+4. Zapisz do pliku csv.
--- node-ftp-listing.js
+++ node-ftp-listing.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- var Client = require('ftp');
+ /*var Client = require('ftp');
 
   var c = new Client();
   c.on('ready', function() {
@@ -19,3 +19,19 @@
   });
   // connect to localhost:21 as anonymous
   c.connect({host: 'ftp.rockerspro.pl', user: 'dziuba', password: 'giwoo9du'});
+*/
+
+// CSV Listing/*
+//Converter Class
+var fs=require("fs");
+var file = "sm.csv";
+
+
+fs.readFile(file, "utf8", function (err, data) {
+    if (err) throw err;
+    var elS;
+    data.forEach(function(el){
+        elS = el.split(';');
+        console.log(elS[0]);
+    });
+});
