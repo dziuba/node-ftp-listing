9065ab96c85a16512d3ee9c0083e727621b8ef39
dziuba <dziuba@agmk.net>
2014-09-08 12:20:59 +0200

    Rozpoznawanie i przypisanie nośników

--- product-prepare-db.js
+++ product-prepare-db.js
@@ -1,28 +1,38 @@
 GLOBAL.prepareProducts = function(products, artists, carriers, callback){
     var data = [];
     var search;
+    var i = 1;
     artists.forEach(function(art){
         var tmp = {
             id: art.id,
             name: art.symfonia_name,
-            products: [],
-            titles: []
+            //products: [],
+            titles: [],
+            carriers: []
         };
         var titles = [];
+        var titlesClean = [];
+        var carriers = [];
+        
         products.forEach(function(prod){
             //if(el.titleBracket !== null) log(el.titleBracket);
             if(!strcmp(art.symfonia_name, prod.artist)){
-                if(!searchFor(titles, prod.titleClean)){
-                    titles.push(prod.titleClean);
-                }
-                tmp.products.push(prod);
+                if(prod.titleClean !== null)
+                    if(!searchFor(titlesClean, prod.titleClean.replace(/ /g, ''))){
+                        titles.push(prod.titleClean);
+                        titlesClean.push(prod.titleClean.replace(/ /g, ''));
+                    }
+                //tmp.products.push(prod);
+                if()
             }
                 
         });
         tmp.titles = titles;
         if(art.symfonia_name === "BETHEL") search = tmp;
         data.push(tmp);
+        
+        if(i === artists.length) callback(search);
+        
+        i++;
     });
-    console.log(search);
-    log(data.length);  
 };
